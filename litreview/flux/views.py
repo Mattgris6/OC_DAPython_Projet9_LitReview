09ff8f5596ddf528ec8ed622f09f52288f0bde34
Matthieu Grison <mat.grison@gmail.com>
2022-02-28 16:39:40 +0100

    Mise en forme des flux

--- litreview/flux/views.py
+++ litreview/flux/views.py
@@ -4,10 +4,23 @@ from . import forms
 from . import models
 from django.shortcuts import get_object_or_404
 
+from itertools import chain
+
+from django.db.models import CharField, Value
+
+
 @login_required
 def home(request):
     tickets = models.Ticket.objects.all()
-    return render(request, 'flux/home.html', context={'tickets': tickets})
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+    reviews = models.Review.objects.all()
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+    posts = sorted(
+        chain(reviews, tickets), 
+        key=lambda post: post.time_created, 
+        reverse=True
+    )
+    return render(request, 'flux/home.html', context={'posts': posts})
 
 @login_required
 def ticket_upload(request):
@@ -49,3 +62,19 @@ def review_upload(request):
     }
     return render(request, 'flux/review_upload.html', context=context)
         
+def feed(request):
+    reviews = get_users_viewable_reviews(request.user)
+    # returns queryset of reviews
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+
+    tickets = get_users_viewable_tickets(request.user) 
+    # returns queryset of tickets
+    tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    # combine and sort the two types of posts
+    posts = sorted(
+        chain(reviews, tickets), 
+        key=lambda post: post.time_created, 
+        reverse=True
+    )
+    return render(request, 'feed.html', context={'posts': posts})
