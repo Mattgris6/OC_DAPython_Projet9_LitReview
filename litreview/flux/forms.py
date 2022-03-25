e557509ac5db7bbedecd52bd1037d66b35a501e5
Matthieu Grison <mat.grison@gmail.com>
2022-03-25 18:17:16 +0100

    Finalisation, affichage de l'étoile de notation, MAJ du formulaire des critiques

--- litreview/flux/forms.py
+++ litreview/flux/forms.py
@@ -5,15 +5,21 @@ from . import models
 
 class TicketForm(forms.ModelForm):
     edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
-
+    title = forms.CharField(label='Titre', max_length=128, widget=forms.TextInput(attrs={'size': '60'}))
+    description = forms.CharField(label='Description', max_length=2048, required=False, widget=forms.Textarea(attrs={'rows': '10', 'cols': '60'}))
+    image = forms.ImageField(label='Image', required=False)
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
 
 
 class ReviewForm(forms.ModelForm):
+    RATINGS = [('0', 0), ('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5)]
     edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
-
+    headline = forms.CharField(label='Titre', max_length=128, widget=forms.TextInput(attrs={'size': '60'}))
+    rating = forms.ChoiceField(label='Note', widget=forms.RadioSelect, choices=RATINGS)
+    body = forms.CharField(label='Commentaire', max_length=8192, required=False,
+                           widget=forms.Textarea(attrs={'rows': '10', 'cols': '60'}))
     class Meta:
         model = models.Review
         fields = ['headline', 'rating', 'body']
